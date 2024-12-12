3ac251c2d8b4033b0373743a2e0cc770f77747f1
ballqs <ballqs_@naver.com>
2024-12-12 17:51:50 +0900

    [update] UserApplication 정리

--- module-user/src/main/java/com/personal/user/UserApplication.java
+++ module-user/src/main/java/com/personal/user/UserApplication.java
@@ -1,10 +1,8 @@
 package com.personal.user;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RequiredArgsConstructor
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
